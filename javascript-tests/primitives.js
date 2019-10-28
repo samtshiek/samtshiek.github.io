701140dc5d001edb23b42e2cde0d55c223542845
Samuel Kamanda on college machine <samueltshiek.kamanda@bellevuecollege.edu>
2019-10-28 12:20:53 -0700

    javascript

---
+++ javascript-tests/primitives.js
@@ -0,0 +1,22 @@
+const myString = "Learning Javascript";
+const myFirstNumericVariable = 3;
+const myBoolean = true;
+const myNull = null;
+let myUndefined = "Learning more javascript";
+
+var oldStyle = "This was the old way to do it.";
+let flexible = "Allows you to change this variable.";
+const constantVariable = "This variable cannot be changed. Except...";
+
+const quotable = 'He said, "Hello". He\'s';
+console.log(myString);
+console.log("TYPE: ", typeof myString);
+console.log(myFirstNumericVariable);
+console.log("TYPE: ", typeof myFirstNumericVariable);
+console.log(myBoolean);
+console.log("TYPE: ", typeof myBoolean);
+console.log(myNull);
+console.log("TYPE: ", typeof myNull);
+console.log(myUndefined);
+console.log("TYPE: ", typeof myUndefined);
+console.log("Add 3 + 25 ", myFirstNumericVariable + 25);
