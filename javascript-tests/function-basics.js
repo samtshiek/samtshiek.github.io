8547e438556a699bdb438029fb7d169acaed0436
Samuel Kamanda on college machine <samtshiek@yahoo.fr>
2019-11-13 12:19:45 -0800

    jscript

--- javascript-tests/function-basics.js
+++ javascript-tests/function-basics.js
@@ -1,7 +1,31 @@
-function simpleFunction() {
-    console.log("This is a simple function");
-}
+window.onload = () => {
+    // All code goes in here.
+    const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
+    const functionCallsAction = document.getElementById('functionCallsAction');
+
+
+    function simpleFunction() {
+        console.log("This is a simple function");
+        simpleFunctionDisplay.textContent = "simple";
+        functionCallsAction.onclick = "Button Action";
+    }
+    simpleFunction();
+
 
 function functionParameters(value) {
     console.log(value);
+}
+function add(a,b) {
+    return a+b;
+}
+function functionReturn(addResult) {
+    return "From function return " + addResult;
+}
+
+
+functionParameters("Many functions take parameters.");
+const addResult = add(2,3);
+console.log(addResult);
+const returnValue = functionReturn(addResult);
+console.log(returnValue);
 }
